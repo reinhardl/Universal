182e316aa39f2ffdd2706c8c49ee3de5174c023b
reinhardl <netzwerk@gmail.com>
2015-10-18 20:16:31 +0200

    -
    Deutsche Strings

---
+++ UTF-8/lang_de.inc.php
@@ -0,0 +1,201 @@
+<?php
+// General
+@define('STARTPAGE_CAT_WINDOWSINFO', '<b><p style="color:#FFFFFF; background-color: blue"> Reihen der Startseite:  </p></b>');
+@define('CATLEAD_TEXTCOUNT','LEAD-Kategorie Zeichenanzahl');
+@define('NAVLINK_INFO2', '<b><p style="color:#FFFFFF; background-color: green"> Navigation2 mit Sublinks </p></b>');
+@define('STARTPAGEROWS_ENABLE','Zeige Reihen mit Kategorieboxen? ');
+@define('NAVLINK_BRAND_LINK','Link des Navigationsnamens');
+@define('NAVLINK_BRAND','Navigationsname');
+@define('ENABLE_NAVLINK_BRAND','Zeige Navigationsnamen');
+@define('NAVLINK_SEARCH','Suche in der Navigation');
+@define('NAVLINK_HOME','Home-Link in der Navigation');
+@define('NAVLINK_ARCHIVE','Archiv-Link in der  Navigation');
+@define('NAVLINK_AMOUNT2','Anzahl der Menüpunkte');
+@define('HAUPTMENUE_TEXT','Text des Haupmenü-Links '); 
+@define('HAUPTMENUE_URL','URL des Haupmenü-Links ');
+@define('HAUPTMENUE_BLANK_TARGET','Öffne Link im neuen Fenster '); 
+@define('SITENAV_SUBNAV_LINK_AMOUNT','Anzahl Untermenü-Links '); 
+@define('SITENAV_SUBNAV_LINK_TEXT' ,'Text des Untermenü-Links '); 
+@define('SITENAV_SUBNAV_LINK_URL' ,'URL des Untermenü-Links '); 
+@define('SITENAV_SUBNAV_LINK_BLANK_TARGET' ,'Öffne Untermenü-Link im neuen Fenster');  
+@define('NAVLINK_ENABLE' ,'Hauptmenü aktiviert ');  
+@define('THEME_HAUPTMENUE' ,'Hauptmenü und Logo ');
+@define('THEME_STARTSEITE' ,'Startseite: Lead Kategorie und Fensterreihen');
+@define('NAVLINK_USE_DESC' ,'Das Hauptmenü ist ein erweitertes Menü welches auch Dropdown Untermenüs hat. Als erstes die "Anzahl der Menüpunkte" festlegen und abspeichern. Dann wieder in Themes konfigurieren und die neuen Menüpunkte (haben einen grünen Balken) mit Inhalt füllen. ');  
+@define('STATIC_SHOW_SIDEBARS_TITLE','Seitenleisten anzeigen?  Falls nein wird die statische Seite in voller Breite ausgegeben.');
+@define('STATIC_SHOW_SIDEBARS_TEXT','Seitenleisten anzeigen?');
+@define('SLIDER_TEXT1','Großer Text im Bild:');
+@define('SLIDER_TEXT2','Kleiner Text im Bild:');
+@define('MOREM','Slider-Bild verlinkt nach:');
+@define('RLSLIDER1_LINK_OPTIONS_TITLE','<b><p style="color:#FFFFFF; background-color: #AC0909">Parameter für Slide </p>');
+@define('SHOW_RIGHTSIDEBAR','Rechte Seitenleiste anzeigen?');
+@define('SHOW_RIGHTSIDEBAR_USE_DESC','Globale Einstellung, ob überhaupt die Seitenleiste angezeigt wird. Übersteuert die Einträge in einzelnen Seiten ');
+@define('THEME_SLIDER_OPTIONS','Startseite: Theme Slider Optionen');
+@define('STARTPAGE_ROWS_USE_DESC','Anzahl der Reihen d.h. Anzahl der Kategorien, die auf der Startseite angezeigt werden.');
+@define('STARTPAGE_REIHE','Anzahl der Reihen mit Kategorieboxen auf der Startseite');
+@define('DISPLAY_CAT_NAME','Kategoriename in den Boxen anzeigen');
+@define('STARTPAGE_INSTRUCTIONS','<strong>Magazin Modus</strong> <br/>Auf der Startseite kann man eine Lead-Kategorie und mehrere "Reihen" mit Kategorieboxen erstellen lassen. Jede Reihe bekommt von Ihnen eine spezielle Kategorie zugeordnet. Zur Zeit hat eine Reihe 3 Boxen. Dieses sind die 3 jüngsten Einträge aus der gewählten Kategorie. Das Bild das in den Boxen angezeigt wird ist bei den "Erweiterte Eigenschaften von Artikeln" unter "EntryPicture" anzugeben. Wird dort kein Bild angegeben wird dieser Eintrag nicht auf der Startseite in der Box dargestellt und der nächste genommen.  ');
+@define('STDNAVIGATION_INSTRUCTIONS','<strong>Globale Navigation</strong> <br/>Die klassische "Globale Navigation" von Serendipity wird von diesem  Template in den Template-Footer angezeigt. ');
+ 
+@define('THE_TITLE','Überschrift der Reihe');
+@define('STARTROW_TRUNCATE_QTY','Anzahl der Zeichen Text in der Box:');
+@define('STARTROW_SHOW_TITLE','Überschrift und Beschreibung der Reihe anzeigen?');
+@define('CATDESCRIPTION','Beschreibung der Reihe');
+@define('STARTROWS_TITLE','Optionen für Reihe Nr.'); 
+@define('STARTROW',' auf der Startseite.'); 
+@define('WIN_COLUMN','Spalten (Kategorieboxen) pro Reihe:');
+@define('THE_DESIGN','Wähle ein Design:');
+@define('STARTROW_ASSIGN_ID','Kategorie der Spalte:');
+
+@define('ERROR_404', 'Fehler 404 - Die angeforderte Seite wurde nicht gefunden.');
+@define('SEARCH_WHAT','Wonach soll gesucht werden?'); //used on quicksearch modal
+@define('SEARCH','Suche');
+@define('TOGGLE_NAV','Navigation'); //only seen by screen readers
+@define('CLOSE','Schließen'); //close button on search form
+@define('READ_MORE','Mehr lesen');
+
+//Option groups and instructions
+@define('THEME_PAGE_OPTIONS', 'Globale-Optionen');
+@define('THEME_HEADERS', 'Header-Bilder');
+@define('THEME_SOCIAL_LINKS', 'Footer: Social-Links');
+@define('THEME_BUSINESS_GALLERY', 'Startseite: Business Galerie Optionen');
+@define('GESAMT', 'GESAMT');
+@define('BLOG_ARCHIVE', 'Blog-Archiv');
+@define('THEME_NAVIGATION', 'Footer-Navigation Optionen');
+@define('SITENAV2_LINK_OPTIONS_TITLE',' Hauptmenü - Menüpunkt:');
+@define('THEME_README','Lies mich');
+@define('THEME_EP_YES','<p>Das Plugin Erweiterte Eigenschaften für Einträge (serendipity_event_entryproperties) wird benötigt. Es ist installiert und aktiv.</p>');
+@define('THEME_EP_NO','<p class="serendipity_msg_important">Das Plugin Erweiterte Eigenschaften für Einträge (serendipity_event_entryproperties) wird benötigt. Es ist entweder nicht installiert oder inaktiv. Bitte das Plugin installieren, um alle Features dieses Themes voll zu nutzen.</p>');
+@define('THEME_INSTRUCTIONS','<p>Dieses Theme nutzt breite Header-Bilder für unterschiedliche Seitentypen. Standardbilder werden mit dem Theme geliefert, diese sind 1900px breit. Diese Standardbilder können unten in den Einstellungen ausgetauscht werden. Zusaätzlich zu den Standardbildern können für jeden Eintrag und jede statische Seite eigene Header-Bilder definiert werden.</p> <p>Das Design dieses Theme sieht die Nutzung eines sekundären Untertitels oder einer Verfasserzeile direkt unter dem Eintragstitel vor. Dieser Untertitel und das Eintragsbild können im Abschnitt "Erweiterte Optionen" beim Anlegen oder Bearbeiten einer Seite geändert werden. Optional kann man das Theme so einstellen, dass die ersten 70 Zeichen des Eintrags als Untertitel verwendet werden, wenn kein Untertitel angegeben wurde.</p> <p>Dieses Theme bietet keine Seitenleisten, kann jedoch optional die Seitenleisten-Plugins Kategorien und Tags in der Archiv-Seite ausgeben. Siehe "' . THEME_PAGE_OPTIONS . '" unten.</p> <p>Falls das Avatar-Plugin (serendipity_event_gravatar) verwendet wird, sollte dessen Option "Smarty Tag erzeugen" für eine optimale Darstellung auf "Ja" gesetzt werden.</p> <p>Die Option "Erweiteres Smarty" in serendipity_event_freetag sollte für eine schönere Darstellung im Eintragsfuß ebenfalls auf "Ja" gesetzt sein.</p>');
+@define('THEME_ENTRY_PROPERTIES_HEADING','Eintragseigenschaften des BASICA-Themes');
+@define('THEME_CUSTOM_FIELD_HEADING', 'Freie Felder für Einträge');
+@define('THEME_CUSTOM_FIELD_DEFINITION','Diese optionalen Felder sind nur verfügbar, wenn dieses Theme (BASICA) verwendet wird. Das Ereignis-Plugin serendipity_event_entryproperties (Erweiterte Eigenschaften für Einträge) muss installiert sein, um diese Felder zu verwenden. Der Eintrags-Untertitel wird unter dem Titel des Eintrags ausgegeben und sollte eine kurze Zusammenfassung des Eintrags enthalten. Das "Bild des Eintrags" wird bei den Startseiten-Modulen "Lead-Kategorie", "Latest-Posts", "Fensterreihen" wie auch bei der Darstellung der Einträge und auf den Archivseiten angezeigt. Bevorzugt hier bitte bei der Bildauswahl die Vorschau wählen.');
+@define('THEME_ENTRY_SUBTITLE','Untertitel des Eintrags');
+@define('THEME_ENTRY_HEADER_IMAGE','Bild des Eintrags');
+
+//Latest Posts
+@define('THEME_LATESTPOSTS','Startseite: Letzte Meldungen');
+@define('LATESTPOST_DESCRIPTION','<strong>Optionen Modul "Letzte Meldungen"</strong><br/>Dieser Bereich blendet zei Spalten auf der Startseite ein, die jeweils eine in der Theme-Konfiguration anzugebene Anzahl an Einträgen einer bestimmten Kategorie anteasert.');
+
+@define('LATEST_POST_ENABLE','Letzte Meldungen anzeigen?');
+@define('LATEST_POST_CAT1','Kategorie der 1. Spalte:');
+@define('LATEST_POST_CAT2','Kategorie der 2. Spalte:');
+@define('LATEST_POST_AMOUNT1','Anzahl Einträge 1. Spalte ');
+@define('LATEST_POST_AMOUNT2','Anzahl Einträge 2. Spalte ');
+@define('LATEST_POST_WITH_PIC1','Bilder in 1. Spalte anzeigen?');
+@define('LATEST_POST_WITH_PIC2','Bilder in 2. Spalte anzeigen?');
+@define('LATEST_POST_HEADER1','Überschrift linke Spalte');
+@define('LATEST_POST_HEADER2','Überschrift rechte Spalte');
+@define('LATEST_POST_TEXTCOUNT1','Zeichen in linker Spalte');
+@define('LATEST_POST_TEXTCOUNT2','Zeichen rechter Spalte');
+//Actionbar
+@define('THEME_ACTIONBAR','Startseite: Actionbar');
+@define('ACTIONBAR_DESCRIPTION','Die Actionbar ist ein einfacher Banner mit Text und einem Link-Buttom mit dem man auf eine besondere Seite hinweisen kann.');
+@define('ENABLE_AKTIONBAR','Actionbar anzeigen?');
+@define('TEXT_AKTIONBAR','Text in der Actionbar:');
+@define('LINKTEXT_AKTIONBAR','Linktext des Buttoms:');
+@define('LINKURL_AKTIONBAR','URL des Buttoms:');
+//workers
+@define('WORKERS_DESCRIPTION','<strong>Die Mitarbeiteranzeige.</strong> <br/> Hier kann man Mitarbeiter mit Foto und Links zu sozialen Netzen wie auch einem Link zu ein Seite im Blog (z.B. Statische Seite oder Eintrag) setzen. Bei leeren Feldern werden die Links nicht angezeigt.');
+@define('LINKTYPE_WEBSW','Gleiches Browserfenster ');
+@define('LINKTYPE_WEBNW','Neues Browserfenster ');
+@define('LINKTYPE_LIGHTBOX','Lightbox (link auf Bild)');
+@define('RWORKER1_TEXT1','Überschrift');
+@define('RWORKER1_TEXT2','Text');
+@define('RWORKER1_LINK1','Link');
+@define('RWORKER1_LINK2','Linktyp');
+@define('BG_IMG','Bild');
+@define('ENABLE_WORKERS','Mitarbeiteranzeige auf Startseite');
+@define('THEME_STARTSEITE_WORKERS','Startseite: Mitarbeiteranzeige');
+@define('WORKER1_AMOUNT','Anzahl anzuzeigende Mitarbeiter');
+@define('RWORKER1_LINK_OPTIONS_TITLE','<b><p style="color:#FFFFFF; background-color: #AC0909"> Mitarbeiter </p>');
+@define('FACEBOOK_LINK1','Facebooklink des Mitarbeiters');
+@define('GITHUB_LINK','Github Link des Mitarbeiters');
+@define('TUMBLR_LINK','Tumblr Link des Mitarbeiters');
+// Lead Kategorie
+@define('LEAD_CAT_WINDOWSINFO', '<b><p style="color:#FFFFFF; background-color: #AC0909"> Angaben zur Lead-Kategorie: </p></b><b>Hinweis:</b> Das BASICA-Template unterstüzt ein begleitendes Bild zu einem Eintrag. Damit dieses Feature funktioniert, muss das s9y-Event-Plugin <i>Erweiterte Eigenschaften von Artikeln</i> installiert sein. Zusätzlich muss in der Konfiguration dieses Plugins unter <i>Freie Felder</i> ein Feld namens <span style="color: #1C45C3"><strong>EntryPicture</strong></span> (Groß-/Kleinschreibung beachten!) angelegt sein. Dann kann beim Erstellen eines Eintrags diesem unter <i>Erweiterte Optionen</i> ein Bild aus der Mediendatenbank zugewiesen werden.&nbsp; <br />');  
+@define('ENABLE_CATLEAD','Lead-Kategorie auf der Startseite anzeigen?');
+@define('CAT_LEAD','Lead-Kategorie auswählen');
+@define('LEAD_INSTRUCTIONS','<strong>Magazin Modus</strong> <br/>Auf der Startseite kann eine Lead-Kategorie angegeben werden. Zudem kann man zusätzlich darunter mehrere "Reihen" mit Kategorieboxen erstellen lassen. Jede Reihe bekommt von Ihnen eine spezielle Kategorie zugeordnet. Zur Zeit hat eine Reihe 3 Boxen. Dieses sind die 3 jüngsten Einträge aus der gewählten Kategorie. Das Bild das in den Boxen angezeigt wird ist bei den "Erweiterte Eigenschaften von Artikeln" unter "EntryPicture" anzugeben. Wird dort kein Bild angegeben wird dieser Eintrag nicht auf der Startseite in der Box dargestellt und der nächste genommen. <br/>Als Erstes gibt man die Anzahl der Reihen an. ');
+
+// Header Images
+@define('DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild');
+@define('DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird auf Seiten verwendet, die kein eigenes Header-Bild definieren.');
+@define('ENTRY_DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild für Einträge');
+@define('ENTRY_DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird für Einträge verwendet, die kein eigenes Header-Bild definieren.');
+@define('STATICPAGE_DEFAULT_HEADER_IMAGE', 'Standard-Header-Bild für statische Seiten');
+@define('STATICPAGE_DEFAULT_HEADER_IMAGE_DESC', 'Dieses Bild wird für statische Seiten verwendet, die kein eigenes Header-Bild definieren.');
+@define('STATICPAGE_SPECIFIC_HEADER_IMAGE','Header-Bild für statische Seiten (optional)');
+@define('CONTACTFORM_HEADER_IMAGE', 'Header-Bild für das Kontaktformular');
+@define('ARCHIVE_HEADER_IMAGE', 'Header-Bild für die Archivseite');
+
+//Page Options
+@define('HOME_LINK_TEXT', 'Text des Home-Links');
+@define('ENTRY_DATE_FORMAT','Datumsformat für Einträge');
+@define('COMMENT_TIME_FORMAT','Zeitformat für Kommentare und Trackbacks');
+@define('WORDS','Textfassung');
+@define('TIMESTAMP','Zeitstempel');
+@define('SUBTITLE_USE_ENTRYBODY','Die ersten 70 Zeichen des Einrags als Untertitel ausgeben, wenn das Feld Untertitel leer gelassen wurde?');
+@define('ENTRYBODY_DETAILED_ONLY','Eintags-Inhalt nur in der Einzelansicht eines Eintrags anzeigen');
+@define('SHOW_COMMENT_LINK','Zeige Link zu den Kommentaren des Eintrags');
+@define('CATEGORIES_ON_ARCHIVE', 'Kategorien auf der Archivseite anzeigen');
+@define('CATEGORIES_ON_ARCHIVE_DESC', 'Das Seitenleisten-Plugin Kategorien (serendipity_plugin_categories) muss installiert und die Option "Smarty-Templating aktivieren?" aktiviert sein, damit Kategorien auf der Archiv-Seite angezeigt werden.');
+@define('TAGS_ON_ARCHIVE', 'Tags auf der Archivseite anzeigen');
+@define('TAGS_ON_ARCHIVE_DESC', 'Das Seitenleisten-Plugin Getaggte Artikel (serendipity_plugin_freetag) muss installiert sein und die Option "Sidebar template" muss auf "archive_freetag.tpl" gesetzt werden, damit Tags auf der Archiv-Seite angezeigt werden.');
+@define('SHOW_PIC_IN_ARCHIVE','Bild in Archiven mit anzeigen?');
+@define('SHOW_PIC_IN_ARCHIVE_DESC','Zeigt das Bild in Blog-Archivseiten mit an, welches unter "Eintrag bearbeiten" dort angegeben wird.');
+//Photogallery in Entries
+@define('THEME_ENTRY_PHOTO_GALLERY','Verzeichnis aus der die Foto-Galerie dieses Eintrags erstellt wird:');
+@define('THEME_ENTRY_PICPOSITION','Position der Fotogalerie. (mögliche Werte:top middle buttom)');
+
+//Google Maps
+@define('GOOGLE_MAP_STATICPAGE_DESC','Die statische Seite bei der die Google Map eingeblendet werden soll. Es geht aber auch die URL für ein Kontaktformular aus dem Kontakt-Plugin.');
+@define('THEME_GOOGLEMAPS','Google Maps');
+@define('GOOGLE_MAP_STATICPAGE','Statische Seite für die Karte');
+@define('GOOGLE_MAP_URL','Google Map URL');
+@define('GOOGLE_MAP_URL_DESC','Dieses ist die URL zur Google Map. Hilfe dazu git es unter Karten einbetten bei der Google Hilfe: https://support.google.com/maps/answer/3544418?hl=de ');
+@define('GOOGLE_MAP_SUPPORT','Google Map Einblendung');
+
+//Static Pages
+@define('STATIC_SHOW_AUTHOR_TEXT','Autorennamen anzeigen');
+@define('STATIC_SHOW_DATE_TEXT','Datum anzeigen');
+
+//Navigation
+@define('USE_CORENAV', 'Globale Navigation verwenden?');
+@define('NAV_POSITION', 'Wo wird sie angezeigt:');
+@define('NAV_HEADLINE', 'Überschrift:');
+@define('NAVPOS_LEFT', 'Links im Footer ');
+@define('NAVPOS_MIDDLE', 'Mitte im Footer');
+@define('NAVPOS_RIGHT', 'Rechts im Footer');
+@define('NAV_HEADLINE_DEF', 'Default Überschrift');
+//Social media
+@define('ENABLE_SOCIAL', 'Social-Links anzeigen?');
+@define('SOCIAL_POSITION', 'Position im Footer:');
+@define('SOCIAL_DESCRIPTION', 'Einleitungstext:');
+@define('SOCIAL_HEADER', 'Überschrift Social Icons:');
+@define('TWITTER_URL','Twitter-URL. Icon wird nicht angezeigt, falls leer.');
+@define('LINKEDIN_URL','Linkedin-URL. Icon wird nicht angezeigt, falls leer.');
+@define('DRIBBBLE_URL','Dribbble-URL. Icon wird nicht angezeigt, falls leer.');
+@define('SKYPE_URL','Skype-URL. Icon wird nicht angezeigt, falls leer.');
+@define('FACEBOOK_URL','Facebook-URL. Icon wird nicht angezeigt, falls leer.');
+@define('RSS_URL','RSS feed-URL. Icon wird nicht angezeigt, falls leer.');
+@define('GITHUB_URL','GitHub-URL. Icon wird nicht angezeigt, falls leer.');
+@define('INSTAGRAM_URL','Instagram-URL. Icon wird nicht angezeigt, falls leer.');
+@define('PINTEREST_URL','Pinterest-URL. Icon wird nicht angezeigt, falls leer.');
+@define('COPYRIGHT', 'Copyright');
+
+//time ago in words function
+@define('ELAPSED_LESS_THAN_MINUTE_AGO','Vor weniger als einer Minute');
+@define('ELAPSED_ONE_MINUTE_AGO','Vor einer Minute');
+@define('ELAPSED_ONE_DAY_AGO','Vor einem Tag');
+@define('ELAPSED_MINUTES_AGO','Vor %s Minuten');
+@define('ELAPSED_HOURS_AGO','Vor %s Stunden');
+@define('ELAPSED_DAYS_AGO','Vor %s Tagen');
+@define('ELAPSED_MONTHS_AGO','Vor %s Monaten');
+@define('ELAPSED_YEARS_AGO','Vor %s Jahren'); //not currently using this, but defining just in case
+@define('ELAPSED_ABOUT_ONE_HOUR_AGO','Vor ungefähr einer Stunde'); // greater than 45 minutes, less than 90 minutes
+@define('ELAPSED_ABOUT_ONE_MONTH_AGO','Vor ungefähr einem Monat'); // greater than 30 days, less than 60 days
+@define('ELAPSED_ABOUT_ONE_YEAR_AGO','Vor ungefähr einem Jahr'); // greater than one year, less than 2 years
+@define('ELAPSED_OVER_YEARS_AGO','Vor mehr als %s Jahren');// greater than 2 years
+
+ 
